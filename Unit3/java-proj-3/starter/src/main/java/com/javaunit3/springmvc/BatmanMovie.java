55de9c40d491bdc3d1808ba44892fd9b68394221
Nara kim <66691530+max1011-em@users.noreply.github.com>
2023-07-19 18:10:35 -0700

    Add files via upload
    upload unit3

---
+++ Unit3/java-proj-3/starter/src/main/java/com/javaunit3/springmvc/BatmanMovie.java
@@ -0,0 +1,15 @@
+package com.javaunit3.springmvc;
+
+public class BatmanMovie implements Movie{
+    public String getTitle() {
+        return "Batman: The Dark Knight";
+    }
+
+    public String getMaturityRating() {
+        return "PG-13";
+    }
+
+    public String getGenre() {
+        return "Action";
+    }
+}
